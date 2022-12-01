5dd949aff7abd43fca6fdf0251539582772437f1
ASBP <s194582@dtu.dk>
2022-12-01 23:51:46 +0100

    Third draft of PostListItem.kt Finally showing values in card. But not layout

--- app/src/main/java/ram/groupfinder/view/components/PostListItem.kt
+++ app/src/main/java/ram/groupfinder/view/components/PostListItem.kt
@@ -5,13 +5,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseAuth
 import ram.groupfinder.database.getUser
 import ram.groupfinder.model.Post
 import ram.groupfinder.util.userFromDocument
@@ -21,53 +22,72 @@ import ram.groupfinder.view.theme.GreenGrey30
 import ram.groupfinder.view.theme.Shapes
 import java.util.Date
 
+var title = "tit"
+var date = "dat"
+var location = "loc"
+var desc = "desc test"
+
 @Composable
 fun PostListItem(post: Post){
-    post.title?.let { Text(text = it) }
-    post.description?.let { Text(text = it)}
+    //post.title?.let { Text(text = it) }
+    //post.description?.let { Text(text = it)}
 
-    /*getUser(post.userId!!).addOnCompleteListener { Task ->
-        if(Task.isSuccessful) {
-            var user = userFromDocument(Task.result)
+    var contactPhone = ""
+    var contactEmail = ""
+    Text(text = contactPhone)
+    Text(text = contactEmail)
 
-            PostListItemLayout(post = post)
 
+    Text(text = title, fontSize = 30.sp)
+    Text(text = date)
+    Text(text = location)
+    desc = Text(text = desc).toString()
 
+    var descrip = post.description?.let { Text(text = it)}
+    //Spacer(modifier = Modifier.height(10.dp))
+
+    /*Card(modifier = Modifier.fillMaxSize()) {
+        Column() {
+            Text(text = "New card")
+            post.description?.let {
+                desc = "Description1:\n$it" }
+            Text(text = "Still new card")
+            descrip
+            post.description?.let { "Description2:\n$it" }
         }
     }*/
-
     Card (modifier = Modifier
-        .fillMaxWidth()
-        .padding(4.dp)
+        .fillMaxSize()
+        .padding(2.dp)
         //.border(width = 4.dp, color = Color.Black, shape = Shapes.medium)
         //.clickable(onClick = )
         .border(2.dp, Green10)
-        .background(color = GreenGrey30)
+        //.background(color = GreenGrey30)
 
     ) {
-        Row(modifier = Modifier, Arrangement.SpaceBetween) {
-            post.title?.let {
-                Text(text = it,
-                    fontSize = 30.sp
-                )
+        Column(modifier = Modifier, Arrangement.SpaceBetween) {
+            getUser(post.userId!!).addOnCompleteListener { Task ->
+                if(Task.isSuccessful) {
+                    var user = userFromDocument(Task.result)
+
+                    post.title?.let { title = "YAY" + it }
+                    post.date?.let { date = "YAYDate: $it" }
+
+                    post.location?.let {
+                        location = "YAYLocation: $it" }
+
+                    post.description?.let {
+                        desc = "YAYDescription:\n $it" }
+                
+                    if (FirebaseAuth.getInstance().currentUser != null) {
+                        contactPhone = "Phonenumber: " + user.phoneNumber
+                        contactEmail = "Phonenumber: " + user.phoneNumber
+                    }
+                }
             }
-            post.date?.let { Text(text = "Date: $it") }
         }
-        post.location?.let {
-            Text(text = "Location: $it")}
-        Spacer(modifier = Modifier.height(10.dp))
-        post.description?.let {
-            Text(text = "Description:\n $it")}
-
     }
+    //post.description?.let { Text(text = it)}
 }
-
-/*@Composable
-fun PostListItemLayout(post: Post){
-    post.title?.let {
-        Text(text = it,
-            fontSize = 10.sp
-        ) }
-}*/
 
 // .paddingFromBaseline(bottom = 80.dp)
