739d4f5fad0e35fb07078ff2152ea857353f8c6c
ASBP <s194582@dtu.dk>
2022-12-01 20:47:16 +0100

    PostListItem.kt layout begun

--- app/src/main/java/ram/groupfinder/view/components/PostListItem.kt
+++ app/src/main/java/ram/groupfinder/view/components/PostListItem.kt
@@ -2,10 +2,26 @@ package ram.groupfinder.view.components
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+import ram.groupfinder.database.getUser
 import ram.groupfinder.model.Post
+import ram.groupfinder.util.userFromDocument
 
 @Composable
 fun PostListItem(post: Post){
     post.title?.let { Text(text = it) }
     post.description?.let { Text(text = it)}
+
+    getUser(post.userId!!).addOnCompleteListener { Task ->
+        if(Task.isSuccessful) {
+            var user = userFromDocument(Task.result)
+
+
+
+        }
+    }
+    post.title?.let {
+        Text(text = it,
+            fontSize = 10.sp
+    ) }
 }
