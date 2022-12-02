fc7e39c0c4d971d59063798e692703b5b058627e
ASBP <s194582@dtu.dk>
2022-12-02 12:04:07 +0100

    Fourth draft of PostListItem.kt

--- app/src/main/java/ram/groupfinder/view/components/PostListItem.kt
+++ app/src/main/java/ram/groupfinder/view/components/PostListItem.kt
@@ -2,92 +2,55 @@ package ram.groupfinder.view.components
 
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.SnackbarDefaults.backgroundColor
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.TextFieldDefaults.BackgroundOpacity
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import ram.groupfinder.database.getUser
 import ram.groupfinder.model.Post
 import ram.groupfinder.util.userFromDocument
-import ram.groupfinder.view.theme.DarkGreen10
-import ram.groupfinder.view.theme.Green10
-import ram.groupfinder.view.theme.GreenGrey30
-import ram.groupfinder.view.theme.Shapes
-import java.util.Date
-
-var title = ""
-var date = ""
-var location = ""
-var desc = ""
+import ram.groupfinder.view.theme.*
 
 @Composable
-fun PostListItem(post: Post){
-    //post.title?.let { Text(text = it) }
-    //post.description?.let { Text(text = it)}
+fun PostListItem(post: Post) {
 
-    var contactPhone = ""
-    var contactEmail = ""
-    Text(text = contactPhone)
-    Text(text = contactEmail)
-
-
-    Text(text = title, fontSize = 30.sp)
-    Text(text = date)
-    Text(text = location)
-    desc = Text(text = desc).toString()
-
-    var descrip = post.description?.let { Text(text = it)}
-    //Spacer(modifier = Modifier.height(10.dp))
-
-    /*Card(modifier = Modifier.fillMaxSize()) {
-        Column() {
-            Text(text = "New card")
-            post.description?.let {
-                desc = "Description1:\n$it" }
-            Text(text = "Still new card")
-            descrip
-            post.description?.let { "Description2:\n$it" }
-        }
-    }*/
-    Card (modifier = Modifier
-        .fillMaxSize()
-        .padding(2.dp)
-        //.border(width = 4.dp, color = Color.Black, shape = Shapes.medium)
-        //.clickable(onClick = )
-        .border(2.dp, Green10)
-        //.background(color = GreenGrey30)
-
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(4.dp)
+            //.clickable(onClick = )
+            .border(2.dp, MaterialTheme.colors.primary, Shapes.medium),
+            backgroundColor = MaterialTheme.colors.onSurface.copy(alpha = BackgroundOpacity) // Same as textField
     ) {
-        Column(modifier = Modifier, Arrangement.SpaceBetween) {
-            getUser(post.userId!!).addOnCompleteListener { Task ->
-                if(Task.isSuccessful) {
-                    var user = userFromDocument(Task.result)
-
-                    post.title?.let { title = "YAY" + it }
-                    post.date?.let { date = "YAYDate: $it" }
-
-                    post.location?.let {
-                        location = "YAYLocation: $it" }
-
-                    post.description?.let {
-                        desc = "YAYDescription:\n $it" }
-                
-                    if (FirebaseAuth.getInstance().currentUser != null) {
-                        contactPhone = "Phonenumber: " + user.phoneNumber
-                        contactEmail = "Phonenumber: " + user.phoneNumber
-                    }
-                }
+        Column(
+            modifier = Modifier
+                .padding(10.dp),
+            Arrangement.SpaceBetween
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                post.title?.let { Text(text = it, fontSize = 23.sp, fontWeight = FontWeight.Bold) }
+                post.date?.let { Text(text = "Posted: ${it.toDate()}", fontSize = 10.sp) }
             }
+            post.location?.let { Text(text = "Location: $it") }
+            post.description?.let { Text(text = "Description:\n$it") }
+
+            //if (FirebaseAuth.getInstance().currentUser != null) {
+            Spacer(modifier = Modifier.height(15.dp))
+            Text(text = "Contact information:", fontWeight = FontWeight.Bold)
+            var contactPhone = Text(text = "Phonenumber: " + "")
+            var contactEmail = Text(text = "Email: " + "")
+            //}
+            /*if (FirebaseAuth.getInstance().currentUser != null) {
+                contactPhone = "Phonenumber: " + user.phoneNumber
+                contactEmail = "Email: " + user.email
+            }*/
         }
     }
-    //post.description?.let { Text(text = it)}
 }
-
-// .paddingFromBaseline(bottom = 80.dp)
